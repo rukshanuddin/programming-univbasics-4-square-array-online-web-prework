3b30961cf5f457f379051e8b2f61799988730388
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-03 22:18:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   counter = 0
-  array = [1, 2, 3]
   while counter < array.length do
-    return array[counter] = array[counter] ** 2
+    array[counter] = array[counter] ** 2
     counter += 1
+    return array
   end
 end
